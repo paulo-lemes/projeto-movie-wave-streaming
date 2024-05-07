3862867095b6201d8905a85cdf66498127e604c3
Paulo Lemes <paulo-lemes@live.com>
2024-05-06 23:41:35 -0300

    Updated .env variable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
-"use client"
-
-import { requestAPI } from "@/api";
-import { useEffect } from "react";
 
 export default function Home() {
-  useEffect(()=>{
-  requestAPI("GET", "authentication");
-  }, [])
 
   return <main className="mb-auto"></main>;
 }
