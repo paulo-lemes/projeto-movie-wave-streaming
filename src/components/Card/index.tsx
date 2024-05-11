48dcc51b8695480e610bf951e1cd73e2ce0d6afd
Paulo Lemes <paulo-lemes@live.com>
2024-05-11 01:24:50 -0300

    Added card info modal styles

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import { content } from "@/types";
 import { Modal } from "../Modal";
 import { useModal } from "@/hooks/useModal";
+import { IoCloseCircleOutline } from "react-icons/io5";
 
 export function Card({
   title,
@@ -29,21 +30,27 @@ export function Card({
           className=""
         />
       </div>
-        <Modal isOpen={isModalOpen} onClose={handleModalClose}>
-          <div>
-            <Image
-              src={`https://image.tmdb.org/t/p/original/${backdrop_path}`}
-              alt={`Capa de ${title || name}`}
-              width={250}
-              height={250}
-              className=""
-            />
-            <div className="flex flex-col">
-              <h4 className="font-bold">{title || name}</h4>
-              <p>{overview}</p>
-            </div>
+      <Modal isOpen={isModalOpen} onClose={handleModalClose}>
+        <div className="bg-black rounded max-w-2xl max-h-[90vh]">
+          <div
+            className="relative float-right -mb-14 mt-2 mr-2 z-50 cursor-pointer"
+            onClick={handleModalClose}
+          >
+            <IoCloseCircleOutline size={30} aria-label="Fechar" />
           </div>
-        </Modal>
+          <Image
+            src={`https://image.tmdb.org/t/p/original/${backdrop_path}`}
+            alt={`Capa de ${title || name}`}
+            width={700}
+            height={250}
+            className="mask max-h-[45vh]"
+          />
+          <div className="flex flex-col p-10 pt-0 max-h-[45vh]">
+            <h4 className="font-bold text-2xl -mt-4 mb-2">{title || name}</h4>
+            <p className="overflow-y-auto">{overview}</p>
+          </div>
+        </div>
+      </Modal>
     </>
   );
 }
