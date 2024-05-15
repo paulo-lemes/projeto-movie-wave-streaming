b3b926e751f5417998f8e2f24c3327a6016284c6
Paulo Lemes <paulo-lemes@live.com>
2024-05-15 13:49:31 -0300

    Updated ContentRow scroll functions and buttons

--- src/components/ContentRow/index.tsx
+++ src/components/ContentRow/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useRef, useState } from "react";
+import React, { UIEvent, useRef, useState } from "react";
 import { dataProps } from "@/types";
 import { Card } from "../Card";
 import { IoMdArrowDropleft, IoMdArrowDropright } from "react-icons/io";
@@ -23,28 +23,35 @@ export function ContentRow({ results, children }: dataProps) {
       const maxWidth = carousel.current.scrollWidth;
       carousel.current.scrollLeft += offset;
       setScrollLeft((prev) =>
-        prev + offset <= maxWidth - offset ? prev + offset : maxWidth
+        prev + offset <= maxWidth - offset ? prev + offset : maxWidth - offset
       );
     }
   };
 
+  const handleScroll = (e: UIEvent) => {
+    const divEventTarget = e.target as HTMLDivElement;
+    setScrollLeft(divEventTarget.scrollLeft);
+  };
+
   return (
     <section className="p-10 pl-0.5 pr-0">
-      <h3 className="font-bold text-4xl mb-3 ml-12 pl-2">{children}</h3>
-      <div className="flex">
+      <h3 className="font-bold text-4xl mb-3 ml-12">{children}</h3>
+      <div className="flex items-center">
         <button
           type="button"
-          className={
-            scrollLeft
-              ? "opacity-50 hover:opacity-100"
-              : "opacity-0 cursor-default"
-          }
+          className={`absolute left-0 h-[384px] bg-gradient-to-l from-transparent to-base-100 ${
+            !scrollLeft && "opacity-0 cursor-default"
+          }`}
           onClick={handleScrollLeft}
         >
-          <IoMdArrowDropleft size={50} />
+          <IoMdArrowDropleft
+            size={50}
+            className="opacity-50 hover:opacity-100"
+          />
         </button>
         <div
-          className="flex flex-col overflow-hidden overflow-x-auto no-scrollbar scroll-smooth"
+          className="flex flex-col overflow-hidden overflow-x-auto no-scrollbar scroll-smooth px-10"
+          onScroll={(e) => handleScroll(e)}
           ref={carousel}
         >
           <div className="w-max flex gap-3 p-1">
@@ -55,14 +62,18 @@ export function ContentRow({ results, children }: dataProps) {
         </div>
         <button
           type="button"
-          className={
-            scrollLeft === carousel.current?.scrollWidth
-              ? "opacity-0 cursor-default"
-              : "opacity-50 hover:opacity-100"
-          }
+          className={`absolute right-0 h-[384px] bg-gradient-to-r from-transparent to-base-100 ${
+            scrollLeft ===
+              (carousel.current &&
+                carousel.current?.scrollWidth -
+                  carousel.current?.offsetWidth) && "opacity-0 cursor-default"
+          }`}
           onClick={handleScrollRight}
         >
-          <IoMdArrowDropright size={50} />
+          <IoMdArrowDropright
+            size={50}
+            className="opacity-50 hover:opacity-100"
+          />
         </button>
       </div>
     </section>
