5c36e92ede8a2b593abc500735ab7db4ff9bf3ba
Paulo Lemes <paulo-lemes@live.com>
2024-05-17 13:11:31 -0300

    Added key to genres button

--- src/components/GenreButton/index.tsx
+++ src/components/GenreButton/index.tsx
@@ -4,9 +4,9 @@ import tmdbGenres from "../../tmdb_genres.json";
 export function GenreButton({ genreId }: { genreId: number }) {
   const genres = tmdbGenres.genres.filter((genre) => genre.id === genreId);
 
-  return genres.map((genre) => (
-    <button className="btn btn-xs btn-neutral disabled w-max text-xs">
-      {genre.name}
+  return genres.map(({id, name}) => (
+    <button key={id} className="btn btn-xs btn-neutral disabled w-max text-xs">
+      {name}
     </button>
   ));
 }
