2205c6f0a115d81ac1801a792285b73818d6ed9e
Paulo Lemes <paulo-lemes@live.com>
2024-05-24 01:56:14 -0300

    Created CreditsContent component

--- src/app/[type]/[id]/page.tsx
+++ src/app/[type]/[id]/page.tsx
@@ -6,6 +6,7 @@ import { ContentVideo } from "@/components/ContentVideo";
 import { Loading } from "@/components/Loading";
 import { ContentRow } from "@/components/ContentRow";
 import { WatchProvider } from "@/components/WatchProvider";
+import { CreditsContent } from "@/components/CreditsContent";
 
 export default async function Content({
   params,
@@ -15,26 +16,35 @@ export default async function Content({
   const { type, id } = params;
 
   const contentDetailsData = getApiContent(`${type}/${id}?language=pt-BR`);
+  const contentCreditsData = getApiContent(`${type}/${id}/credits?language=pt-BR`);
   const contentVideosData = getApiContent(`${type}/${id}/videos?language=pt-BR`);
   const contentProvidersData = getApiContent(`${type}/${id}/watch/providers`);
   const recommendedContentData = getApiContent(`${type}/${id}/recommendations?language=pt-BR&page=1`);
 
-  const [contentDetails, contentVideos, contentProviders, recommendedContent] =
-    await Promise.all([
-      contentDetailsData,
-      contentVideosData,
-      contentProvidersData,
-      recommendedContentData,
-    ]);
+  const [
+    contentDetails,
+    contentCredits,
+    contentVideos,
+    contentProviders,
+    recommendedContent,
+  ] = await Promise.all([
+    contentDetailsData,
+    contentCreditsData,
+    contentVideosData,
+    contentProvidersData,
+    recommendedContentData,
+  ]);
 
   console.log(contentDetails);
+  console.log(contentCredits);
   console.log(contentVideos);
-  console.log(contentProviders.results.BR);
+  console.log(contentProviders);
   console.log(recommendedContent);
 
   return contentDetails.id ? (
     <FadeInContent duration={1.5}>
       <ContentDetails {...contentDetails} />
+      <CreditsContent {...contentCredits} />
       <WatchProvider {...contentProviders.results.BR} />
       <ContentVideo {...contentVideos} />
       <ContentRow {...recommendedContent}>Recomendados</ContentRow>
---
+++ src/components/CreditsContent/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { credits } from "@/types";
+
+export function CreditsContent({ cast, crew }: credits) {
+  const actors = cast.slice(0, 10);
+  const directors = crew.filter(
+    ({ job }) =>
+      job.toLowerCase() === "director" ||
+      job.toLowerCase() === "executive producer"
+  );
+
+  console.log(actors);
+  console.log(directors);
+
+  return (
+    <div className="flex flex-col gap-1 px-4 sm:px-16 mb-6">
+      <div className="flex flex-wrap gap-1">
+        {actors.map(({ name }) => (
+          <p>{name}</p>
+        ))}
+      </div>
+      <div className="flex flex-wrap gap-1">
+        {directors.map(({ name }) => (
+          <p>{name}</p>
+        ))}
+      </div>
+    </div>
+  );
+}
