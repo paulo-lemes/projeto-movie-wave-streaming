2def8befa0f6a7b57980a09d6617e87446ad90ed
Paulo Lemes <paulo-lemes@live.com>
2024-06-25 21:34:37 -0300

    Removed unused daisyui themes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   },
   plugins: [require("daisyui")],
   daisyui: {
-    themes: ["black", "autumn", "forest", "luxury"],
+    themes: ["forest"],
   },
 };
 export default config;
