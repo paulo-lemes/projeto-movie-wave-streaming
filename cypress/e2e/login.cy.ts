b324a6c0bf16633ae233da5385886ee2948c40b4
Paulo Lemes <paulo-lemes@live.com>
2024-06-26 23:25:04 -0300

    Added invalid credentials login test

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,12 +1,24 @@
 describe("Login spec", () => {
-  it("should not pass with empty credentials", () => {
-    cy.visit("http://localhost:3000");
+  beforeEach(() => cy.visit("http://localhost:3000"));
 
+  it("should not pass with empty credentials", () => {
     cy.getByData("user-options-button").click();
     cy.getByData("login-anchor").click();
 
     cy.location("pathname").should("eq", "/login");
 
+    cy.getByData("enter-button").click();
+    cy.getByData("modal-text").contains("Usuário e/ou senha inválidos");
+  });
+
+  it("should not pass with invalid credentials", () => {
+    cy.getByData("user-options-button").click();
+    cy.getByData("login-anchor").click();
+
+    cy.location("pathname").should("eq", "/login");
+
+    cy.getByData("username-input").type("abc");
+    cy.getByData("password-input").type("123");
     cy.getByData("enter-button").click();
     cy.getByData("modal-text").contains("Usuário e/ou senha inválidos");
   });
