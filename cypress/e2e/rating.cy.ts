35c7a124c1d2cda5a1339a00e426c17f46c3cdda
Paulo Lemes <paulo-lemes@live.com>
2024-07-01 14:58:44 -0300

    Added contexts, without login test

--- cypress/e2e/rating.cy.ts
+++ cypress/e2e/rating.cy.ts
@@ -1,41 +1,59 @@
 describe("Rating content spec", () => {
-  beforeEach(() => {
-    cy.login();
-    cy.visit("/");
-    cy.getByData("content-row").eq(1).find("a").eq(0).click();
-    cy.url().should("include", "/movie");
+  context("With login", () => {
+    beforeEach(() => {
+      cy.login();
+      cy.visit("/");
+      cy.getByData("content-row").eq(1).find("a").eq(0).click();
+      cy.url().should("include", "/movie");
+    });
+
+    it("should rate a content properly", () => {
+      cy.getByData("rate-button")
+        .should("exist")
+        .and("contain.text", "Avaliar")
+        .click();
+      cy.wait(1000);
+      cy.getByData("rating-star").eq(9).click();
+      cy.getByData("send-rating-button").click();
+      cy.verifyAndCloseModal("Avaliação feita!");
+      cy.getByData("user-rating").should("contain.text", "10/10");
+    });
+
+    it("should change a rating properly", () => {
+      cy.getByData("change-rating-button")
+        .should("exist")
+        .and("contain.text", "Alterar")
+        .click();
+      cy.wait(1000);
+      cy.getByData("rating-star").eq(0).click();
+      cy.getByData("send-rating-button").click();
+      cy.verifyAndCloseModal("Avaliação atualizada!");
+      cy.getByData("user-rating").should("contain.text", "1/10");
+    });
+
+    it("should delete a rating properly", () => {
+      cy.getByData("change-rating-button")
+        .should("exist")
+        .and("contain.text", "Alterar")
+        .click();
+      cy.getByData("delete-rating-button").click();
+      cy.verifyAndCloseModal("Avaliação excluída");
+      cy.getByData("rate-button")
+        .should("exist")
+        .and("contain.text", "Avaliar");
+    });
   });
 
-  it("should rate a content properly", () => {
-    cy.getByData("rate-button")
-      .should("exist")
-      .and("contain.text", "Avaliar")
-      .click();
-    cy.getByData("rating-star").eq(9).click();
-    cy.getByData("send-rating-button").click();
-    cy.verifyAndCloseModal("Avaliação feita!");
-    cy.getByData("user-rating").should("contain.text", "10/10");
-  });
-
-  it("should change a rating properly", () => {
-    cy.getByData("change-rating-button")
-      .should("exist")
-      .and("contain.text", "Alterar")
-      .click();
-    cy.wait(100);
-    cy.getByData("rating-star").eq(0).click();
-    cy.getByData("send-rating-button").click();
-    cy.verifyAndCloseModal("Avaliação atualizada!");
-    cy.getByData("user-rating").should("contain.text", "1/10");
-  });
-
-  it("should delete a rating properly", () => {
-    cy.getByData("change-rating-button")
-      .should("exist")
-      .and("contain.text", "Alterar")
-      .click();
-    cy.getByData("delete-rating-button").click();
-    cy.verifyAndCloseModal("Avaliação excluída");
-    cy.getByData("rate-button").should("exist").and("contain.text", "Avaliar");
+  context("Without login", () => {
+    it("shouldn't allow rate action", () => {
+      cy.visit("/");
+      cy.getByData("content-row").eq(1).find("a").eq(0).click();
+      cy.url().should("include", "/movie");
+      cy.getByData("rate-button")
+        .should("exist")
+        .and("contain.text", "Avaliar")
+        .click();
+      cy.verifyAndCloseModal("Faça login para conseguir avaliar conteúdos");
+    });
   });
 });
