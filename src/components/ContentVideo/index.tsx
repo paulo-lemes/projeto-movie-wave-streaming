d1b32df4340968ff4a41813264b0bb190d20f66e
Paulo Lemes <paulo-lemes@live.com>
2024-07-06 12:57:12 -0300

    Added title prop to ContentVideo iframe

--- src/components/ContentVideo/index.tsx
+++ src/components/ContentVideo/index.tsx
@@ -9,9 +9,10 @@ export function ContentVideo({ results }: { results: VideoProps[] }) {
       <section className="flex flex-col gap-4 px-4 sm:px-16 mb-6">
         <h4 className="text-2xl font-bold">Multimídia</h4>
         <div className="flex flex-wrap gap-4">
-          {videos.map(({ key }) => (
+          {videos.map(({ key, name }) => (
             <iframe
               key={key}
+              title={name}
               src={`https://www.youtube.com/embed/${key}`}
               allowFullScreen
               width={1920}
